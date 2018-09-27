945d9f6615b4937e1250680f230ec46b53176604
XRaiderGH <37107080+XRaiderGH@users.noreply.github.com>
2018-09-27 11:36:11 +0200

    Deze stank is nu nog beter

---
+++ src/Practicum.java
@@ -0,0 +1,20 @@
+public class Practicum {
+
+    public static void main(String[] args) {
+
+        boolean janAlleman = true;
+
+        char eenLetter = 'a';
+        String stukjeTekst = "Wat Leuk";
+
+        float puntGetal = 1.0f;
+        double groterPuntGetal = 1.0d;
+
+        System.out.println(janAlleman);
+        System.out.println(eenLetter);
+        System.out.println(stukjeTekst);
+        System.out.println(puntGetal);
+        System.out.println(groterPuntGetal);
+
+    }
+}
